d434a6203833f3faa81c9dd9461e367c52a70267
Jialaiz <scuzjl@hotmail.com>
2024-08-08 01:42:32 +1000

    Site updated: 2024-08-08 01:42:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/06/WineAus出口中国指南/","2024/08/04/常用话术以及邮件格式/","2024/08/05/红酒相关术语/"];function toRandomPost(){
+var posts=["2024/08/04/常用话术以及邮件格式/","2024/08/06/WineAus出口中国指南/","2024/08/05/红酒相关术语/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
