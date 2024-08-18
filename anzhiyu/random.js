baa74cc30369203e2604c3d4793700a824451956
JialaiZhang <66731934+JialaiZhang@users.noreply.github.com>
2024-08-18 06:31:01 +0000

    NEW POST STABLEDIFFUSION101 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/11/Cubase101(壹)/","2024/08/12/Cubase中Effect Track的各种FX/","2024/08/06/WineAus出口中国指南/","2024/08/09/低成本高效搭建个人博客网站/","2024/08/04/常用话术以及邮件格式/","2024/08/05/红酒相关术语/","2024/08/13/红酒运输和相关文件/"];function toRandomPost(){
+var posts=["2024/08/11/Cubase101(壹)/","2024/08/12/Cubase中Effect Track的各种FX/","2024/08/18/Stable Diffusion101(壹)/","2024/08/06/WineAus出口中国指南/","2024/08/09/低成本高效搭建个人博客网站/","2024/08/04/常用话术以及邮件格式/","2024/08/05/红酒相关术语/","2024/08/13/红酒运输和相关文件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
